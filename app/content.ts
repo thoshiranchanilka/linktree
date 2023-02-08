5769a334a1627818e7b9362e5f77a9f2cbd434e7
thoshiranchanilka <bgtch8356@gmail.com>
2023-02-08 16:48:05 +0530

    Linktree by Pruthivi Thejan

---
+++ app/content.ts
@@ -0,0 +1,58 @@
+export const codeRepository = "linktree";
+export const githubUsername = "thoshiranchanilka";
+export const heading = "Thoshiran Chanilka | Linktree";
+export const slogan = {
+  words: "I'll be myself or",
+  hightlight: "die trying",
+};
+
+export const Cards = [
+  {
+    href: "https://mobile.twitter.com/toshiranC",
+    icon: "twitter",
+    title: "Twitter",
+    description: "I care about Twitter, I hope Elon Musk will not ruin it.",
+  },
+  {
+    href: "https://www.instagram.com/toshiranc/",
+    icon: "instagram",
+    title: "Instagram",
+    description: "When I take good photos, I post it here.",
+  },
+  {
+    href: "https://t.me/ThoshiranChanilka/",
+    icon: "telegram",
+    title: "Telegram",
+    description: "Telegram is my most used messaging app.",
+  },
+  {
+    href: "https://link.friendspire.com/kilVKnTH8wb",
+    icon: "friendspire",
+    title: "Friendspire",
+    description: "Movies, TV Shows, Books and Podcasts are rated here.",
+  },
+  {
+    href: "https://open.spotify.com/user/31w6qmwwldvidci4ca64d7uaoqrq",
+    icon: "spotify",
+    title: "Spotify",
+    description: "My Spotify playlists are here.",
+  },
+  {
+    href: "https://github.com/thoshiranchanilka",
+    icon: "github",
+    title: "Github",
+    description: "I upload all my codes here.",
+  },
+  {
+    href: "https://facebook.com/toshiran.chanilka",
+    icon: "facebook",
+    title: "Facebook",
+    description: "I don't use facebook much.",
+  },
+  {
+    href: "mailto:toshiranchanilka@gmail.com",
+    icon: "gmail",
+    title: "Gmail",
+    description: "For business enquiries",
+  },
+];
