832738185fd4c5edaa8071691df2ece8a09d6b94
thoshiranchanilka <bgtch8356@gmail.com>
2023-02-09 15:59:08 +0530

    update

--- app/content.ts
+++ app/content.ts
@@ -2,8 +2,8 @@ export const codeRepository = "linktree";
 export const githubUsername = "thoshiranchanilka";
 export const heading = "Thoshiran Chanilka | Linktree";
 export const slogan = {
-  words: "I'll be myself or",
-  hightlight: "die trying",
+  words: "Nothing is impossible if you",
+  hightlight: "just Imagined.",
 };
 
 export const Cards = [
@@ -11,43 +11,49 @@ export const Cards = [
     href: "https://mobile.twitter.com/toshiranC",
     icon: "twitter",
     title: "Twitter",
-    description: "I care about Twitter, I hope Elon Musk will not ruin it.",
+    description: "I mostly use twitter for crypto news.",
   },
   {
     href: "https://www.instagram.com/toshiranc/",
     icon: "instagram",
     title: "Instagram",
-    description: "When I take good photos, I post it here.",
+    description: "I use insta to showcase my photographs.",
   },
   {
     href: "https://t.me/ThoshiranChanilka/",
     icon: "telegram",
     title: "Telegram",
-    description: "Telegram is my most used messaging app.",
+    description: "Drop a text if you want any movie.",
   },
   {
     href: "https://link.friendspire.com/kilVKnTH8wb",
     icon: "friendspire",
     title: "Friendspire",
-    description: "Movies, TV Shows, Books and Podcasts are rated here.",
+    description: "Movies, TV Shows that I watched are rated here.",
   },
   {
     href: "https://open.spotify.com/user/31w6qmwwldvidci4ca64d7uaoqrq",
     icon: "spotify",
     title: "Spotify",
-    description: "My Spotify playlists are here.",
+    description: "My Spotify playlists are here.( I'm not a artist. LOL..)",
   },
   {
     href: "https://github.com/thoshiranchanilka",
     icon: "github",
     title: "Github",
-    description: "I upload all my codes here.",
+    description: "I'm not a professional programmer but when I need help with codes I use GitHub.",
   },
   {
     href: "https://facebook.com/toshiran.chanilka",
     icon: "facebook",
     title: "Facebook",
-    description: "I use facebook but not too much.",
+    description: "I use facebook but not too much. It's toxic",
+  },
+  {
+    href: "https://www.behance.net/thoshirachanilka",
+    icon: "behance",
+    title: "Behance",
+    description: "This is where I put my all the Designs and Projects.",
   },
   {
     href: "mailto:toshiranchanilka@gmail.com",
